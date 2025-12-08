14b65a2afd1c7cebbfc1ee294202c35d6863f3e1
Nawin Kishore <151768353+Nawinkishore@users.noreply.github.com>
2025-12-08 23:08:51 +0530

    commit

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -7,6 +7,8 @@ export const generateTokenAndSetCookie = (res, userId) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        domain : ".onrender.com",
+        path : '/'
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
 };
